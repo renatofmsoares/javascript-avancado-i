952de72f0780f054dc92cb78d9d8bda931fe4d49
Renato Soares <renatof.m.soares@gmail.com>
2019-04-20 00:29:49 -0300

    alterando sintaxe get

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -8,19 +8,19 @@ class Negociacao {
 
     }
 
-    getVolume() {
+    get volume() {
         return this._quantidade * this._valor;
     }
     
-    getData() {
+    get data() {
         return this._data;
     }
     
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade;
     }
     
-    getValor() {
+    get valor() {
         return this._valor;
     }
 }
