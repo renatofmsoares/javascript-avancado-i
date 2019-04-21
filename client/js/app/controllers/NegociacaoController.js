f1598ec1c6537c16ee394490d65d7f16bb73c03d
Renato Soares <renatof.m.soares@gmail.com>
2019-04-21 14:03:00 -0300

    Criando alias para o 'document.querySelector usando o bind(document)

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,9 +2,10 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
 
-        let inputData = document.querySelector('#data');
-        let inputQuantidade = document.querySelector('#quantidade');
-        let inputValor = document.querySelector('#valor');
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
 
         console.log(inputData.value);
         console.log(inputQuantidade.value);
