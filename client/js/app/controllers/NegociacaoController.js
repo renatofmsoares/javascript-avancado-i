bb913e9efaa43ce5ae4b62053d72fa889196f2ff
Renato Soares <renatosoares@softbox.com.br>
2019-04-26 15:14:52 -0300

    adicionando mensagem

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,13 +5,19 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
     }
 
     adiciona(event){
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso.'
+        this._mensagemView.update(this._mensagem);
         this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
         console.log(this._listaNegociacoes)
