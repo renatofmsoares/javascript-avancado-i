83f1b9a773521a4c90dc2c94c81433b3df91b2b0
Renato Soares <renatosoares@softbox.com.br>
2019-04-26 11:58:30 -0300

    adicionando ListaNegociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,6 +4,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
@@ -15,7 +16,7 @@ class NegociacaoController {
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data))
+        this._listaNegociacoes.adiciona(negociacao);
+        console.log(this._listaNegociacoes)
     }
 }
